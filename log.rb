b179ed6e5971389cfe52762f047aa4f04fae1997
Paweł Pacana <pawel.pacana@gmail.com>
2024-02-07 22:04:44 +0100

    Add xmin implementation
    https://github.com/RailsEventStore/rails_event_store/issues/106#issuecomment-370268384

--- log.rb
+++ log.rb
@@ -23,6 +23,38 @@ consumer =
 
     loop do
       results = $db[:log].where { id > last_id }.order(:id).to_a
+
+      if results.empty?
+        last_id == producer_count * batch_size ? break : next
+      else
+        last_id, count = [results.last[:id], results.size + count]
+
+        print "last_id: #{last_id}, consumed: #{count}/#{producer_count * batch_size}\r"
+      end
+      sleep 0.001
+    end
+    puts
+  end
+
+xmin_consumer =
+  lambda do
+    $barrier.wait
+    puts "consumer.#{SecureRandom.hex(2)} started"
+
+    last_id, count = 0, 0
+
+    loop do
+      results =
+        $db[:log]
+          .where(
+            Sequel.lit(
+              "id > ? AND xmin::text < txid_snapshot_xmin(txid_current_snapshot())::text",
+              last_id
+            )
+          )
+          .order(:id)
+          .to_a
+
       if results.empty?
         last_id == producer_count * batch_size ? break : next
       else
@@ -67,8 +99,9 @@ serialized_producer =
   end
 
 {
-  "ordinary producer" => [producer, consumer],
-  "serialized producer" => [serialized_producer, consumer]
+  "ordinary producer, ordinary consumer" => [producer, consumer],
+  "serialized producer, ordinary consumer" => [serialized_producer, consumer],
+  "ordinary producer, xmin consumer" => [producer, xmin_consumer]
 }.each do |name, (producer, consumer)|
   puts
   puts "--- #{name} ---"
