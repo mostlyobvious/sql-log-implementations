2fd7a3dbd039121026d54a266b97be87653e7bc2
Paweł Pacana <pawel.pacana@gmail.com>
2024-02-14 09:57:28 +0100

    Now, that's a test

--- log_test.rb
+++ log_test.rb
@@ -7,87 +7,128 @@ gemfile do
 end
 
 class LogTest < Minitest::Test
-  class Actor
-    def initialize(mk_connection, name)
-      @pg = mk_connection.call
-      @name = name
+  def simple_reader
+    lambda do |connection, last_id|
+      rows =
+        connection
+          .exec_params(
+            "SELECT id FROM log WHERE id > $1 ORDER BY id",
+            [last_id]
+          )
+          .map { |row| row.fetch("id").to_i }
+      [rows.last, rows]
     end
-
-    def begin = @pg.exec("BEGIN")
-    def insert = @pg.exec_params("INSERT INTO log (name) VALUES ($1)", [@name])
-    def commit = @pg.exec("COMMIT")
   end
+
+  def test_no_overlap_scenario_simple_reader
+    run_lifecycle do |connection|
+      scenario, consumer = mk_no_overlap_scenario, mk_consumer(connection)
+
+      3.times do
+        scenario.resume
+        consumer.call(simple_reader)
+      end
+
+      assert_equal 2, consumer.last_id
+      assert_equal [1, 2], consumer.consumed_ids
+    end
+  end
+
+  def test_overlap_scenario_simple_reader
+    run_lifecycle do |connection|
+      scenario, consumer = mk_overlap_scenario, mk_consumer(connection)
+
+      3.times do
+        scenario.resume
+        consumer.call(simple_reader)
+      end
+
+      assert_equal 2, consumer.last_id
+      assert_equal [1, 2], consumer.consumed_ids
+    end
+  end
+
+  private
 
   # kaka:  [ 1  ]
   # dudu:   [ 2   ]
   # query:     Q Q Q
-  def test_no_overlap
-    run_lifecycle do |pg|
-      kaka, dudu = mk_actor("kaka"), mk_actor("dudu")
+  def mk_no_overlap_scenario
+    kaka, dudu = mk_actor, mk_actor
+    Fiber.new do
+      [kaka, dudu, kaka, dudu].each { _1.resume }
+      Fiber.yield
 
-      kaka.begin
-      dudu.begin
+      kaka.resume
+      Fiber.yield
 
-      kaka.insert
-      dudu.insert
-      assert_empty visible_rows
-
-      kaka.commit
-      assert_equal [{ 1 => "kaka" }], visible_rows
-
-      dudu.commit
-      assert_equal [{ 1 => "kaka" }, { 2 => "dudu" }], visible_rows
+      dudu.resume
     end
   end
 
   # kaka:  [   2 ]
   # dudu:   [ 1    ]
   # query:      Q Q Q
-  def test_overlap
-    run_lifecycle do |pg|
-      kaka, dudu = mk_actor("kaka"), mk_actor("dudu")
+  def mk_overlap_scenario
+    kaka, dudu = mk_actor, mk_actor
+    Fiber.new do
+      [kaka, dudu, dudu, kaka].each { _1.resume }
+      Fiber.yield
 
-      kaka.begin
-      dudu.begin
+      kaka.resume
+      Fiber.yield
 
-      dudu.insert
-      kaka.insert
-      assert_empty visible_rows
-
-      kaka.commit
-      assert_equal [{ 2 => "kaka" }], visible_rows
-
-      dudu.commit
-      assert_equal [{ 1 => "dudu" }, { 2 => "kaka" }], visible_rows
+      dudu.resume
     end
-  end
-
-  private
-
-  def visible_rows
-    mk_connection
-      .call
-      .exec("SELECT id, name FROM log")
-      .map { |row| { row.fetch("id").to_i => row.fetch("name") } }
   end
 
   def run_lifecycle
-    pg = mk_connection.call
-    pg.exec("CREATE TABLE log (id serial primary key, name varchar)")
-    yield pg
+    connection = mk_connection.call
+    connection.exec("CREATE TABLE log (id serial primary key, name varchar)")
+    yield connection
   rescue PG::QueryCanceled => e
   ensure
-    pg.exec("DROP TABLE log")
+    connection.exec("DROP TABLE log")
   end
 
-  def mk_actor(name) = Actor.new(mk_connection, name)
-
-  def mk_connection =
+  def mk_connection
     -> do
-      PG
-        .connect(dbname: "log")
-        .tap do |connection|
-          connection.exec("SET idle_in_transaction_session_timeout = 1000")
-        end
+      connection = PG.connect(dbname: "log")
+      connection.exec("SET idle_in_transaction_session_timeout = 1000")
+      connection
     end
+  end
+
+  def mk_actor
+    Fiber.new do
+      connection = mk_connection.call
+      connection.exec("BEGIN")
+      Fiber.yield
+      connection.exec("INSERT INTO log DEFAULT VALUES")
+      Fiber.yield
+      connection.exec("COMMIT")
+    end
+  end
+
+  def mk_consumer(connection)
+    Class
+      .new do
+        attr_reader :last_id, :consumed_ids
+
+        def initialize(connection)
+          @connection = connection
+          @last_id = 0
+          @consumed_ids = []
+        end
+
+        def call(implementation)
+          last_id, consumed_ids = implementation.call(@connection, @last_id)
+          return if consumed_ids.empty?
+
+          @last_id = last_id
+          @consumed_ids.concat(consumed_ids)
+        end
+      end
+      .new(connection)
+  end
 end
